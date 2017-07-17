963e7d4acd2555e9e80f92a3ab03efd1e1a8e874
reifnotreef <bridgerrhammond@gmail.com>
2017-07-17 10:37:50 -0400

    Fixed navigation being hidden on large screens

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,16 @@
-$('.skip').click(function() {
-    $('.navTopItem').toggle();
-})
-$('.navTopItem').click(function(){
-    $('.navTopItem').toggle();
-});
-$(document).click(function(event) { 
-    if(!$(event.target).closest('.skip').length) {
-        if($('.navTopItem').is(":visible")) {
-            $('.navTopItem').hide();
+// Using $(window).width so make sure navigation is not hidden on click when screen width is 768 or higher
+if ( $(window).width() < 767) {      
+    $('.skip').click(function() {
+        $('.navTopItem').toggle();
+    })
+    $('.navTopItem').click(function(){
+        $('.navTopItem').toggle();
+    });
+    $(document).click(function(event) { 
+        if(!$(event.target).closest('.skip').length) {
+            if($('.navTopItem').is(":visible")) {
+                $('.navTopItem').hide();
+            }
         }
-    }        
-})
+    })
+}
