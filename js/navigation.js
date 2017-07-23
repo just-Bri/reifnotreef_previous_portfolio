3a8e567828d8bc311e289f343e704bfd361c4104
reifnotreef <bridgerrhammond@gmail.com>
2017-07-23 18:55:39 -0400

    Indentation changes

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,16 @@
 // Using $(window).width to make sure navigation is not hidden on click when screen width is 768 or higher
-if ( $(window).width() < 767) {      
-    $('.skip').click(function() {
-        $('.navTopItem').toggle();
-    });
-    $('.navTopItem').click(function(){
-        $('.navTopItem').toggle();
-    });
-    $(document).click(function(event) { 
-        if(!$(event.target).closest('.skip').length) {
-            if($('.navTopItem').is(":visible")) {
-                $('.navTopItem').hide();
-            }
-        }
-    });
+if ($(window).width() < 767) {
+  $('.skip').click(function() {
+    $('.navTopItem').toggle();
+  });
+  $('.navTopItem').click(function() {
+    $('.navTopItem').toggle();
+  });
+  $(document).click(function(event) {
+    if (!$(event.target).closest('.skip').length) {
+      if ($('.navTopItem').is(":visible")) {
+        $('.navTopItem').hide();
+      }
+    }
+  });
 }
--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,10 @@
 $('.projectContainer').hover(
-    function() {
-        $(this).children('a').fadeTo(0.25, 0.25);
-        $(this).children('figcaption').css("display","block");
-    }, function() {
-        $(this).children('a').fadeTo(0.25, 1);
-        $(this).children('figcaption').css("display","none");
-    }
+  function() {
+    $(this).children('a').fadeTo(0.25, 0.25);
+    $(this).children('figcaption').css("display", "block");
+  },
+  function() {
+    $(this).children('a').fadeTo(0.25, 1);
+    $(this).children('figcaption').css("display", "none");
+  }
 );
